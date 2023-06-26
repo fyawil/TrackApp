7c242f8f9fe49ae8bb5a64aacca51c468a542add
Fuad Yusuf Awil <fawil@live.co.uk>
2023-06-26 23:41:51 +0100

    Removed DB creation and display functions and added variable and increment button of variable

--- track_app/app/views.py
+++ track_app/app/views.py
@@ -1,72 +1,15 @@
-def check_table_existence():
-    conn = sqlite3.connect('db.sqlite3')
-    cursor = conn.cursor()
-
-    # Check if the table exists
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='your_table_name'")
-    table_exists = cursor.fetchone() is not None
-
-    conn.close()
-
-    return table_exists
-
-# def get_table_data():
-#     conn = sqlite3.connect('db.sqlite3')
-#     cursor = conn.cursor()
-
-#     # Assuming the table name is 'your_table_name'
-#     cursor.execute("SELECT * FROM your_table_name")
-#     table_data = cursor.fetchall()
-
-#     conn.close()
-
-#     return table_data
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-import sqlite3
 
 def index(request):
-    table_data = None
+
+    count = request.session.get('count', 0)  # Get the current count from session, default to 0
 
     if request.method == 'POST':
-        table_name = request.POST['table_name']
-        create_table(table_name)
+        count += 1  # Increment the count when the form is submitted
 
-    if request.method == 'GET':
-        # Print the table and its values if it exists
-        if check_table_existence():
-            table_data = get_table_data()
-            print(table_data)
-        else:
-            print("Table does not exist.")
-    
-    context = {
-        'table_data': table_data
-    }
-
-    return render(request, 'index.html', context)
-
-def create_table(table_name):
-    conn = sqlite3.connect('db.sqlite3')
-    cursor = conn.cursor()
-    
-    # Create the table
-    cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY, name TEXT)")
-    
-    conn.commit()
-    conn.close()
-
-# The create_table() and check_table_existence() functions remain the same
-
-def get_table_data():
-    conn = sqlite3.connect('db.sqlite3')
-    cursor = conn.cursor()
-
-    # Assuming the table name is 'your_table_name'
-    cursor.execute("SELECT * FROM your_table_name")
-    table_data = cursor.fetchall()
-
-    conn.close()
-
-    return table_data
+        request.session['count'] = count  # Update the count in session
+        return redirect('index')  # Redirect to GET request after successful POST
+    else:
+        count = request.session.get('count', 0)  # Get the current count from session, default to 0
+    return render(request, 'index.html', {'count': count})
