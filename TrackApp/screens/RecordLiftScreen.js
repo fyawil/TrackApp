d2d8827ff9ee41afe32422c3fbd879aa5307bab2
Fuad Yusuf Awil <fawil@live.co.uk>
2023-09-28 15:01:22 +0100

    Added SQLite dat

--- TrackApp/screens/RecordLiftScreen.js
+++ TrackApp/screens/RecordLiftScreen.js
@@ -1,77 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, StatusBar, Pressable, TextInput } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function RecordLiftScreen({ navigation }) {
+
   const [exercise, setExerciseText] = useState('');
   const [day, setDayText] = useState('');
   const [month, setMonthText] = useState('');
   const [year, setYearText] = useState('');
   const [weight, setWeightText] = useState('');
   const [reps, setRepsText] = useState('');
-  const [records, setRecords] = useState([]);
-  const [showRecords, setShowRecords] = useState(false);
-
-  useEffect(() => {
-    loadRecords();
-  }, []);
-
-  const loadRecords = async () => {
-    try {
-      const savedRecords = await AsyncStorage.getItem('liftRecords');
-      if (savedRecords !== null) {
-        setRecords(JSON.parse(savedRecords));
-      }
-    } catch (error) {
-      console.error('Error loading records:', error);
-    }
-  };
-
-  const saveRecords = async () => {
-    try {
-      await AsyncStorage.setItem('liftRecords', JSON.stringify(records));
-    } catch (error) {
-      console.error('Error saving records:', error);
-    }
-  };
-
-  const handleAddRecord = () => {
-    const newRecord = {
-      exercise,
-      date: `${day}/${month}/${year}`,
-      weight,
-      reps,
-    };
-
-    setRecords([...records, newRecord]);
-
-    setExerciseText('');
-    setDayText('');
-    setMonthText('');
-    setYearText('');
-    setWeightText('');
-    setRepsText('');
-
-    saveRecords();
-  };
-
-  const renderRecords = () => {
-    if (showRecords) {
-      return (
-        <View>
-          {records.map((record, index) => (
-            <View key={index}>
-              <Text style={styles.whiteText}>Exercise: {record.exercise}</Text>
-              <Text style={styles.whiteText}>Date: {record.date}</Text>
-              <Text style={styles.whiteText}>Weight: {record.weight}</Text>
-              <Text style={styles.whiteText}>Reps: {record.reps}</Text>
-            </View>
-          ))}
-        </View>
-      );
-    }
-    return null;
-  };
 
   return (
     <View style={styles.container}>
@@ -84,21 +21,18 @@ export default function RecordLiftScreen({ navigation }) {
         <View style={styles.date}>
           <TextInput
             style={styles.inputDay}
-            onChangeText={setDayText}
             value={day}
             placeholder="dd"
             placeholderTextColor="grey"
           />
           <TextInput
             style={styles.inputMonth}
-            onChangeText={setMonthText}
             value={month}
             placeholder="mm"
             placeholderTextColor="grey"
           />
           <TextInput
             style={styles.inputYear}
-            onChangeText={setYearText}
             value={year}
             placeholder="yyyy"
             placeholderTextColor="grey"
@@ -106,32 +40,25 @@ export default function RecordLiftScreen({ navigation }) {
         </View>
         <TextInput
           style={styles.inputExercise}
-          onChangeText={setExerciseText}
           value={exercise}
           placeholder="lift"
           placeholderTextColor="grey"
         />
         <TextInput
           style={styles.inputWeight}
-          onChangeText={setWeightText}
           value={weight}
           placeholder="weight"
           placeholderTextColor="grey"
         />
         <TextInput
           style={styles.inputReps}
-          onChangeText={setRepsText}
           value={reps}
           placeholder="reps"
           placeholderTextColor="grey"
         />
-        <Pressable style={styles.addSetButton} onPress={handleAddRecord}>
+        <Pressable style={styles.addSetButton}>
           <Text style={{ textAlign: "center", fontSize: 24 }}>+</Text>
         </Pressable>
-        <Pressable style={styles.showRecordsButton} onPress={() => setShowRecords(!showRecords)}>
-          <Text style={{ textAlign: "center", fontSize: 24 }}>Show Records</Text>
-        </Pressable>
-        {renderRecords()}
       </View>
       <View style={styles.homePageBottom}>
         <View style={styles.contactUsView}>
@@ -171,13 +98,6 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center",
     justifyContent: "space-evenly",
-  },
-  date: {
-    display: "flex",
-    flexDirection: "row",
-    height: "10%",
-    width: "50%",
-    justifyContent: "space-between",
   },
   inputDay: {
     width: '30%',
@@ -232,15 +152,6 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     borderRadius: 5,
     justifyContent: "center",
-  },
-  showRecordsButton: {
-    padding: 15,
-    backgroundColor: "white",
-    borderRadius: 5,
-    justifyContent: "center",
-  },
-  whiteText: {
-    color: 'white',
   },
   homePageBottom: {
     display: "flex",
--- TrackApp/screens/SignInScreen.js
+++ TrackApp/screens/SignInScreen.js
@@ -1,10 +1,132 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button, Platform } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+import { useState, useEffect } from 'react';
 
 export default function SignInScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Sign In Screen</Text>
-    </View>
-  );
-}
+  
+  // expo add expo-sqlite
+  // expo add expo-file-system
+  // expo add expo-document-picker
+  // expo add expo-sharing
+  // expo add expo-dev-client
+  
+  /*
+    For testing expo-document-picker on iOS we need a standalone app 
+    which is why we install expo-dev-client
+    
+    If you don't have eas installed then install using the following command:
+    npm install -g eas-cli
+  
+    eas login
+    eas build:configure
+  
+    Build for local development on iOS or Android:
+    eas build -p ios --profile development --local
+    OR
+    eas build -p android --profile development --local
+  
+    May need to install the following to build locally (which allows debugging)
+    npm install -g yarn
+    brew install fastlane
+  
+    After building install on your device:
+    For iOS (simulator): https://docs.expo.dev/build-reference/simulators/
+    For Android: https://docs.expo.dev/build-reference/apk/
+  
+    Run on installed app:
+    expo start --dev-client
+  */
+    const [db, setDb] = useState(SQLite.openDatabase('example.db'));
+    const [isLoading, setIsLoading] = useState(true);
+    const [names, setNames] = useState([]);
+    const [currentName, setCurrentName] = useState(undefined);
+  
+    useEffect(() => {
+      db.transaction(tx => {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)')
+      });
+  
+      db.transaction(tx => {
+        tx.executeSql('SELECT * FROM names', null,
+          (txObj, resultSet) => setNames(resultSet.rows._array),
+          (txObj, error) => console.log(error)
+        );
+      });
+  
+      setIsLoading(false);
+    }, [db]);
+  
+    if (isLoading) {
+      return (
+        <View style={styles.container}>
+          <Text>Loading names...</Text>
+        </View>
+      );
+    }
+  
+    const addName = () => {
+      db.transaction(tx => {
+        tx.executeSql('INSERT INTO names (name) values (?)', [currentName],
+          (txObj, resultSet) => {
+            let existingNames = [...names];
+            existingNames.push({ id: resultSet.insertId, name: currentName});
+            setNames(existingNames);
+            setCurrentName(undefined);
+          },
+          (txObj, error) => console.log(error)
+        );
+      });
+    }
+  
+    const deleteName = (id) => {
+      db.transaction(tx => {
+        tx.executeSql('DELETE FROM names WHERE id = ?', [id],
+          (txObj, resultSet) => {
+            if (resultSet.rowsAffected > 0) {
+              let existingNames = [...names].filter(name => name.id !== id);
+              setNames(existingNames);
+            }
+          },
+          (txObj, error) => console.log(error)
+        );
+      });
+    };
+  
+    const showNames = () => {
+      return names.map((name, index) => {
+        return (
+          <View key={index} style={styles.row}>
+            <Text>{name.name}</Text>
+            <Button title='Delete' onPress={() => deleteName(name.id)} />
+          </View>
+        );
+      });
+    };
+  
+    return (
+      <View style={styles.container}>
+        <TextInput value={currentName} placeholder='name' onChangeText={setCurrentName} />
+        <Button title="Add Name" onPress={addName} />
+        {showNames()}
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    row: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      alignSelf: 'stretch',
+      justifyContent: 'space-between',
+      margin: 8
+    }
+  });
+
